8ca8ba99a57464e001a062143b75db3ffefffe7c
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2020-07-24 23:24:47 -0300

    Avançando com Microservices Aula 05 (Conclusão)
    Autenticação e Autorização
    
    O que foi ensinado:
    
        - Como funciona a autenticação e autorização com microsserviços
        - Como criar um servidor de autenticação com Spring Security e Spring Cloud OAuth2
        - A implementação da integração entre o Spring Security e o Spring Cloud OAuth2
        - Como gerar e validar tokens no servidor de autenticação
        - A integração dos microsserviços com o servidor de autenticação
        - Como repassar o token de autenticação através do Zuul
        - A implementar um filtro do Feign para repassar o token do usuário nas chamadas para os microsserviços Fornecedor e Transportador

---
+++ src/main/java/br/com/home/microservice/auth/config/WebSecurityConfigurer.java
@@ -0,0 +1,39 @@
+package br.com.home.microservice.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		return super.userDetailsService();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("doug")
+			.password(passwordEncoder().encode("dougpwd"))
+			.roles("USER");
+	}
+
+}
